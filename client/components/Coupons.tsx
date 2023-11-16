aa42f4489e57017f00ca24ff508f6577e60e6add
student <student@pop-os.localdomain>
2023-11-17 11:18:47 +1300

    Models & Coupon set up

---
+++ client/components/Coupons.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import styles from './PokemonListItem.module.css'
+// API Import here
+import { getFruits } from '../apis/fruits'
+import { useMutation, QueryClient, useQueryClient } from '@tanstack/react-query'
+
+export default function CouponList() {
+  const {
+    data: coupon,
+    error,
+    isLoading,
+  } = useQuery({ queryKey: ['coupon'], queryFn: () => getFruits() }) //Get fruits will be the api function for coupons
+  if (error) return <p>My Brother in Christ</p>
+
+  if (isLoading)
+    return (
+      <>
+        <p>bruh dis shit loading</p>
+      </>
+    )
+}
